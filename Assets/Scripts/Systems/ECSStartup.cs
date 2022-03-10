fb1b4ec2235f847255a9aa603185cf72f3b1e288
dix1mm <dix1mm@mail.ru>
2022-03-10 13:44:27 +0400

    Определена модульная архитектура

--- Assets/Scripts/Systems/ECSStartup.cs
+++ Assets/Scripts/Systems/ECSStartup.cs
@@ -13,21 +13,21 @@ public class ECSStartup : MonoBehaviour{
 	}
 	
 	private void Start(){
-        _world = new EcsWorld();
+		_world = new EcsWorld();
 		_systems = new EcsSystems(_world);
 		_systems.ConvertScene();
 		systemsAdd();
-        _systems.Init();
-    }
+		_systems.Init();
+	}
     
 	private void Update(){
 		_systems.Run();
-    }
+	}
 	
 	private void OnDestroy(){
-        _systems.Destroy();
+		_systems.Destroy();
 		_systems = null;
 		_world.Destroy();
 		_world = null;
-    }
+	}
 }
